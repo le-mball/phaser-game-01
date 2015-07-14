51df90c08dfe5a1f60ec8a3f7a6bfbe4d6344bc5
Matt Ball <mball@lizearle.com>
2015-07-14 13:28:48 +0100

    code dump

---
+++ public/javascripts/game.js
@@ -0,0 +1,45 @@
+/* global Phaser */
+(function(){
+	
+	var game;	
+	var player;
+	var cursors;
+	
+	function preload() {
+    	game.load.image('star', 'images/star.png');
+	}
+	
+	function create() {
+		player = game.add.sprite(0, 0, 'star');		
+		game.physics.arcade.enable(player);		
+		cursors = game.input.keyboard.createCursorKeys();
+	}
+	
+	function update() {
+		player.body.velocity.x = 0;
+		player.body.velocity.y = 0;
+		
+		if(cursors.left.isDown) {
+			player.body.velocity.x = -150;
+		}
+		
+		if(cursors.right.isDown) {
+			player.body.velocity.x = 150;
+		}
+		
+		if(cursors.up.isDown) {
+			player.body.velocity.y = -150;
+		}
+		
+		if(cursors.down.isDown) {
+			player.body.velocity.y = 150;
+		}
+	}
+	
+	game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+		preload: preload,
+		create: create,
+		update: update
+	});
+	
+})();
